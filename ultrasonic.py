f2b7625206ec05c8d02603ee1bb041d00bcb981d
Sangyong Lee <forearth@hanmail.net>
2021-07-07 11:13:38 +0900

    Update ultrasonic.py
    들여쓰기 수정 및 주석추가

--- ultrasonic.py
+++ ultrasonic.py
@@ -11,17 +11,17 @@ echo = Pin(2, Pin.IN)
 
 def ultra():
     trigger.low()
-    utime.sleep_us(2)
+    utime.sleep_us(2)    #sleep_us(2) = 2 microsecond 즉 0.000002
     trigger.high()
     utime.sleep_us(5)
     trigger.low()
     
     while echo.value() == 0:
-        signaloff = utime.ticks_us()
+        signaloff = utime.ticks_us()    # ticks_us()는 microsecond 단위로 시간을 측정한다. 즉, 초음파를 보냈을때의 시간을 계산한다. ex)87906770
     while echo.value() == 1:
-       signalon = utime.ticks_us()
-    timepassed = signalon - signaloff
-    distance = (timepassed * 0.0343) / 2
+        signalon = utime.ticks_us()     # 초음파가 어딘가에 부딪혀 반사된 시점의 시간을 계산한다. ex)87906955
+    timepassed = signalon - signaloff   # 초음파가 보내지고, 반사된 시간차이를 계산한다.    ex)185 microsecond
+    distance = (timepassed * 0.0343) / 2 # 음파의 속도는 343m/s 
 
     print("The distance from object is ",distance,"cm")
 
